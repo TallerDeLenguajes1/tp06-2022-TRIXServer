d72fad738ab9f30cd774024fbb12b11cb8cae497
TRIXServer <admin@trixserver.com>
2022-06-14 15:44:14 -0300

    commit

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿
+classCalculadora calculadora = new classCalculadora();
+
+int seleccion = 10;
+
+do
+{
+    Console.Clear();
+    Console.WriteLine("1. SUMA");
+    Console.WriteLine("2. RESTA");
+    Console.WriteLine("3. MULTIPLICACION");
+    Console.WriteLine("4. DIVISION");
+    Console.WriteLine("--");
+    Console.WriteLine("0. SALIR");
+    Console.WriteLine("--");
+    Console.Write("Ingrese su opcion: ");
+    seleccion = int.Parse(Console.ReadLine());
+    
+} while (seleccion != 0);
+
---
+++ classCalculadora.cs
@@ -0,0 +1,38 @@
+﻿public class classCalculadora
+{
+    public double resultado;
+
+    public classCalculadora(double valor)
+    {
+        resultado = valor;
+    }
+
+    public classCalculadora()
+    {
+    }
+
+    public void suma(double valor)
+    {
+        resultado += valor;
+    }
+
+    public void resta(double valor)
+    {
+        resultado -= valor;
+    }
+
+    public void multiplicacion(double valor)
+    {
+        resultado *= valor;
+    }
+
+    public void dividir(double valor)
+    {
+        resultado = valor != 0 ? resultado /= valor : 0;
+    }
+
+    public void limpiar()
+    {
+        resultado = 0;   
+    }
+}
