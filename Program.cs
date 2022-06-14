b2cd6aa331f9f1875d415701db5eb564959bbcf2
TRIXServer <admin@trixserver.com>
2022-06-14 16:51:39 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+public class empleado
+{
+    private int id;
+    private string nombre;
+    private string apellido;
+    private DateTime fechaNacimiento;
+    private char estadoCivil;
+    private char genero;
+    private DateTime fechaIngreso;
+    private double sueldoBasico;
+    private classCargo cargos;
+
+}
+
+
