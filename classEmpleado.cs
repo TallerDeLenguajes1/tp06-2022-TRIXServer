dcaba3f4e90b9d91bc19533d84501d048951b9af
TRIXServer <admin@trixserver.com>
2022-06-21 15:26:12 -0300

    commit

--- classEmpleado.cs
+++ classEmpleado.cs
@@ -3,23 +3,66 @@ public class empleado
     private int idEmpleado;
     private string nombre;
     private string apellido;
-    private DateTime fechaNacimiento;
-    private char estadoCivil;
-    private char genero;
-    private DateTime fechaIngreso;
+    private DateOnly fechaNacimiento;
+    private string estadoCivil;
+    private string genero;
+    private DateOnly fechaIngreso;
     private double sueldoBasico;
-    private string cargos;
+    private string cargo;
 
     public int IdEmpleado { get => idEmpleado; set => idEmpleado = value; }
     public string Nombre { get => nombre; set => nombre = value; }
     public string Apellido { get => apellido; set => apellido = value; }
-    public DateTime FechaNacimiento { get => fechaNacimiento; set => fechaNacimiento = value; }
-    public char EstadoCivil { get => estadoCivil; set => estadoCivil = value; }
-    public char Genero { get => genero; set => genero = value; }
-    public DateTime FechaIngreso { get => fechaIngreso; set => fechaIngreso = value; }
+    public DateOnly FechaNacimiento { get => fechaNacimiento; set => fechaNacimiento = value; }
+    public string EstadoCivil { get => estadoCivil; set => estadoCivil = value; }
+    public string Genero { get => genero; set => genero = value; }
+    public DateOnly FechaIngreso { get => fechaIngreso; set => fechaIngreso = value; }
     public double SueldoBasico { get => sueldoBasico; set => sueldoBasico = value; }
-    public string Cargos { get => cargos; set => cargos = value; }
+    public string Cargo { get => cargo; set => cargo = value; }
 
-    
+    public empleado(string dataNombre, string dataApellido, char dataEstadoCivil, char dataGenero, int dataCargo, DateOnly dataFechaNacimiento, DateOnly dataFechaIngreso, double dataSueldoBasico)
+    {
+        nombre = dataNombre;
+        apellido = dataApellido;
+        estadoCivil = Convert.ToString((estadoCivil)dataEstadoCivil);
+        genero = Convert.ToString((genero)dataGenero);
+        cargo = Convert.ToString((cargo)dataCargo);
+        fechaNacimiento = dataFechaNacimiento;
+        fechaIngreso = dataFechaIngreso;
+        sueldoBasico = dataSueldoBasico;
+
+    }
+
 }
 
+class constantes
+{
+    public const int cantidadEmpleados = 3;
+
+}
+
+public enum cargo
+{
+    Auxiliar = 1,
+    Administrativo = 2,
+    Ingeniero = 3,
+    Especialista = 4,
+    Investigador = 5
+
+}
+
+public enum estadoCivil
+{
+    Soltero = 'S',
+    Pareja = 'P',
+    Casado = 'C',
+    Viudo = 'V'
+
+}
+
+public enum genero
+{
+    Masculino = 'M',
+    Femenino = 'F'
+
+}
